614ff8781556bd4d884d6494fa9550930e834997
Trando <loic.coulon@condorcet.be>
2018-04-19 21:15:36 +0200

    Ajout de documentation pour pilote et course

--- src/projet/Course.java
+++ src/projet/Course.java
@@ -11,66 +11,114 @@ import java.util.*;
  *
  * @author Trando
  */
+
 public class Course {
 
     protected String ville, pays;
     protected double km;
     protected int ordre;
     protected List<Pilote> participant = new ArrayList<>();
-
+    /**
+     * Constructeur par défaut
+     */
     public Course() {
 
     }
-
+    /**
+     * Constructeur par défaut avec paramètres
+     * @param ville ville ou se déroule la course
+     * @param pays pays ou se déroule la course
+     * @param km taille du circuit en km
+     * @param ordre numéro d'ordre de la course
+     */
     public Course(String ville, String pays, double km, int ordre) {
         this.ville = ville;
         this.pays = pays;
         this.km = km;
         this.ordre = ordre;
     }
+    /**
+     * constructeur avec pour seul parametre l'odre pour la recherche
+     * @param ordre ordre de la course recherchée
+     */
     public Course(int ordre){
         this.ordre=ordre;
     }
-
+    /**
+     * Méthode renvoyant la ville de la course
+     * @return la ville de la course
+     */
     public String getVille() {
         return ville;
     }
-
+    /**
+     * Méthode ranvoyant le pays de la course
+     * @return nom du pays de la course
+     */
     public String getPays() {
         return pays;
     }
-
+    /**
+     * Méthode renvoyant la taille de la course
+     * @return taille de la course 
+     */
     public double getKm() {
         return km;
     }
-
+    /**
+     * méthode renvoyant le numéro d'odre de la course
+     * @return ordre de la course
+     */
     public int getOrdre() {
         return ordre;
     }
-
+    /**
+     * Méthode renvoyant la liste des participants
+     * @return une liste de pilote participant à la course
+     */
     public List<Pilote> getParticipant() {
         return participant;
     }
-
+    /**
+     * Méthode changeant la ville de la course
+     * @param ville Nouvelle ville ou se déroule la course
+     */
     public void setVille(String ville) {
         this.ville = ville;
     }
-
+    /**
+     * Méthode changeant le pays de la course
+     * @param pays Nouveaux pays ou se déroule la course
+     */
     public void setPays(String pays) {
         this.pays = pays;
     }
-
+    /**
+     * Méthode changeant la taille du circuit 
+     * @param km taille du circuit en km
+     */
     public void setKm(double km) {
         this.km = km;
     }
-
+    /**
+     * Méthode changeant le numéro d'ordre de la course
+     * @param ordre nouvelle ordre de la course 
+     */
     public void setOrdre(int ordre) {
         this.ordre = ordre;
     }
-
+    /**
+     * Méthode changeant la liste des participants
+     * @param participant Nouvelle liste des participants
+     */
     public void setParticipant(List<Pilote> participant) {
         this.participant = participant;
     }
+    /**
+     * Méthode ajoutant un participant à la liste des participant de la course
+     * @param p un pilote qui sera ajouté à la liste des participant
+     * @return une chaine indiquant la réussite ou non de l'ajout
+     */
     public String ajoutParticipant(Pilote p){
          if (p == null) {
             return "pilote nul";
@@ -81,7 +129,10 @@ public class Course {
         participant.add(p);
         return "ajout pilote effectue";
     }
-
+    /**
+     * Méthode toString de l'objet Course
+     * @return 
+     */
     @Override
     public String toString() {
         return "Course" + ordre + "se déroule à " + ville + " en " + pays + " avec une taille de " + km + " les participant sont" + participant;
--- src/projet/Pilote.java
+++ src/projet/Pilote.java
@@ -13,50 +13,86 @@ public class Pilote {
 
     protected String nom, prenom, nation, license;
 
+    /**
+     * Constructeur par défaut
+     */
     public Pilote() {
 
     }
-
+    /**
+     * Constructeur avec paramètres
+     * @param nom nom du pilote
+     * @param prenom prenom du pilote
+     * @param nation nation du pilote
+     * @param license license du pilote
+     */
     public Pilote(String nom, String prenom, String nation, String license) {
         this.nom = nom;
         this.license = license;
         this.nation = nation;
         this.prenom = prenom;
     }
-
+    /**
+     * Méthode renvoyant le nom du pilote
+     * @return nom du pilote
+     */
     public String getNom() {
         return nom;
     }
-
+    /**
+     * méthode renvoyant le prenom du pilote
+     * @return prenom du pilote
+     */
     public String getPrenom() {
         return prenom;
     }
-
+    /**
+     * méthode renvoyant la nation du pilote
+     * @return nation du pilote
+     */
     public String getNation() {
         return nation;
     }
-
+    /**
+     * méthode renvoyant la license du pilote
+     * @return license du pilote
+     */
     public String getLicense() {
         return license;
     }
-
+    /**
+     * méthode permettant de changer le nom
+     * @param nom nouveau nom du pilote
+     */
     public void setNom(String nom) {
         this.nom = nom;
     }
-
+    /**
+     * méthode permettant de changer le prenom du pilote
+     * @param prenom nouveau prenom du pilote
+     */
     public void setPrenom(String prenom) {
         this.prenom = prenom;
     }
-
+    /**
+     * méthode permettant de changer la nation du pilote
+     * @param nation nouvelle nation du pilote
+     */
     public void setNation(String nation) {
         this.nation = nation;
     }
-
+    /**
+     * méthode permettant de changer de changer la license du pilote
+     * @param license nouvelle license du pilote
+     */
     public void setLicense(String license) {
         this.license = license;
     }
-
-    @Override
+    /**
+     * méthode toString de l'objet pilote
+     * @return une chaine avec toutes les info du pilotes
+     */
+    @Override //TODO faire une meilleur toString
     public String toString() {
         return "nom = " + nom + ", prenom = " + prenom + ", nation = " + nation + ", license = " + license;
     }
